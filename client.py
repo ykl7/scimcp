092d651c550bdffb6f942419834681317c0e4656
Yash <byashwanth77@gmail.com>
2025-09-24 15:05:54 -0400

    Added google scholar and semantic scholar tools

--- client.py
+++ client.py
@@ -1,33 +1,33 @@
 import os
-from smolagents import CodeAgent, InferenceClientModel
+from smolagents import CodeAgent
 from smolagents.models import AzureOpenAIServerModel
 from smolagents.tools import ToolCollection
 from mcp import StdioServerParameters
-import asyncio
 
-def sync_agent_run():
-
+def main():
     model = AzureOpenAIServerModel(
         model_id="gpt-4",
         azure_endpoint=os.getenv("AZURE_ENDPOINT"),
-        api_key = os.getenv("AZURE_API_KEY"),
+        api_key=os.getenv("AZURE_API_KEY"),
         api_version="2025-01-01-preview"
     )
-    #model = InferenceClientModel() Please uncomment this, im using Azure as i run outof free tokens on inference
-
+    
     server_params = StdioServerParameters(
         command="python",
         args=["server.py"],
         env={**os.environ}
     )
-
+    
     with ToolCollection.from_mcp(server_params, trust_remote_code=True) as tool_collection:
-        agent = CodeAgent(tools=tool_collection.tools, model=model)
-        return agent.run("What's the weather in Stony Brook right now?")
-
-async def main():
-    result = await asyncio.to_thread(sync_agent_run)
-    print(result)
+        agent = CodeAgent(
+            tools=tool_collection.tools, 
+            model=model,
+            additional_authorized_imports=['json']
+            # system_prompt =
+        )
+        
+        result = agent.run("Hello! im yash. Please provide me papers on using agentic ai in material sciences")
+        print(result)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
--- server.py
+++ server.py
@@ -1,34 +1,140 @@
 from fastmcp import FastMCP
-import requests
+from langchain.tools import Tool
+from semanticscholar import SemanticScholar
+from langchain_community.utilities.google_scholar import GoogleScholarAPIWrapper
+from langchain_community.tools.google_scholar import GoogleScholarQueryRun
+from typing import List, Dict
 import os
+import requests
 
-mcp = FastMCP("My MCP Server")
+mcp = FastMCP("Weather_agent")
+
+# def google_scholar_search():
+#     google_scholar_search = GoogleScholarQueryRun(api_wrapper=GoogleScholarAPIWrapper(serp_api_key=os.environ["SERP_API_KEY"]))
+#     return google_scholar_search
+
+# tool = Tool(
+#     name="Google Scholar Search",
+#     func=google_scholar_search().run,
+#     description="Useful for searching academic papers and articles on Google Scholar."
+# )
+
+
+def extract_google_scholar_papers(data: dict):
+    papers = []
+    for result in data.get("organic_results", []):
+        title = result.get("title", "N/A")
+        link = result.get("link", "N/A")
+        snippet = result.get("snippet", "N/A")
+
+        authors_list = result.get("publication_info", {}).get("authors", [])
+        authors = ", ".join([author.get("name", "") for author in authors_list])
+        resources = result.get("resources", [])
+        venue = resources[0].get("title") if resources else "N/A"
+        cited = result.get("inline_links", {}).get("cited_by", {}).get("total", "")
+        summary = result.get("publication_info", {}).get("summary", "")
+        year = "N/A"
+        for token in summary.split():
+            if token.isdigit() and len(token) == 4:   # Just getting the year
+                year = token
+                break
+
+        papers.append({
+            "title": title,
+            "authors": authors,
+            "year": year,
+            "abstract": snippet,
+            "venue": venue,
+            "cited_by": cited,
+            "link": link
+        })
+
+    return papers
+
+# @mcp.tool
+# def Google_scholar_search(topic:str) -> str:
+#     """
+#     This function is used for searching academic papers and articles on Google scholar
+#     Args:
+#          Topic for which the api should search the papers on, this can also include author and domain specifications
+#     Output:
+#          Returns the top matching papers with metadata such as abstract, title, authors, publication year, and link.
+#     """
+#     return tool.run(topic)
+
 
 @mcp.tool
-def greet(name: str) -> str:
-    return f"Hello, {name}!"
-
-@mcp.tool
-def get_weather(city: str) -> str:
+def Google_Scholar_search(topic: str, num_results: int = 5) -> list[dict]:
     """
-    This tool provides the weather of a chosen city using the OpenWeatherMap API.
-    Args:
-        city: str
-    Output:
-        City: temperature, condition
-    """
-    api = os.environ("WEATHER_API_KEY")  # <-- keep private in real projects
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api}&units=metric"
-
-    response = requests.get(url)
-    data = response.json()
-
-    if data.get("cod") != 200:
-        return f"Error: {data.get('message')}"
+    This tool queries Google Scholar via SerpAPI to return research papers. Always start Research papers query with this
     
-    temp = data["main"]["temp"]
-    cond = data["weather"][0]["description"]
-    return f"{city}: {temp}°C, {cond}"
+    Args:
+        topic: The search query for academic papers.
+        num_results: Number of top papers to return.
+
+    Returns:
+            A list of dictionaries for each paper with:
+            - title
+            - abstract
+            - authors (list of dicts with name)
+            - year
+            - venue
+            - citationCount
+            - url (main link)
+    """
+    params = {
+        "engine": "google_scholar",
+        "q": topic,
+        "num": num_results,
+        "api_key": os.environ["SERP_API_KEY"]
+    }
+
+    response = requests.get("https://serpapi.com/search", params=params)
+    response.raise_for_status()
+
+    data = response.json()
+    filtered_metadata = extract_google_scholar_papers(data)
+
+    return filtered_metadata
+
+
+@mcp.tool
+def Semantic_Scholar_search(query="", result_limit=5) -> list[dict]:
+    """
+    This tool retrieves relevant research papers from Semantic Scholar based on a user-defined query.
+
+    Args:
+        query (str): The search query string, which can include keywords, author names, fields of study, publication venues, or other metadata.
+        result_limit (int): The maximum number of papers to return (default is 5).
+
+    Output:
+        A list of the top matching papers with rich metadata for each, including:
+            - title
+            - abstract
+            - citation count
+            - tldr (short summary if available)
+            - fields of study
+            - publication year
+            - authors
+            - open access status
+            - direct link to PDF (if available)
+            - paper URL
+            - venue (conference or journal name)
+        
+    This tool is useful for literature reviews, sourcing papers for research, or finding up-to-date work on a specific topic.
+    """
+    # api_key = os.environ["SEMANTIC_SCHOLAR_API"]
+    fields = "title,abstract,citationCount,tldr,fieldsOfStudy,year,authors,isOpenAccess,openAccessPdf,url,venue"
+
+    rsp = requests.get('https://api.semanticscholar.org/graph/v1/paper/search',
+                           headers={'X-API-KEY': "aBwBbmCpic50CTnKIAXYCuDAIJiUoQaaEbKhsQye"},
+                           params={'query': query, 'limit': result_limit, 'fields': fields})
+
+    rsp.raise_for_status()
+    results = rsp.json()
+
+    return results.get("data", [])
+
 
 if __name__ == "__main__":
     mcp.run()
